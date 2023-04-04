855719fd670291a76e22108df023fc5ae9775a02
Alisa Hamilton <67701544+hamilton89@users.noreply.github.com>
2023-04-04 17:29:14 -0400

    Add files via upload

--- Code/Malaria_LineGraphs.R
+++ Code/Malaria_LineGraphs.R
@@ -11,7 +11,7 @@ library(scales)
 library(reshape2)
 
 # set directory
-setwd("[Main file path]")
+setwd("/Users/alisahamilton/Library/CloudStorage/OneDrive-CenterforDiseaseDynamics,Economics&Policy/HIV Malaria Vaccine/2. Code/")
 
 # load data
 Malaria <- read_csv("Results/Malaria_ByYear.csv")
@@ -28,12 +28,12 @@ CasesAvt = Malaria %>% filter(outcome %in% c('I_avd_'))
 CasesAvtPlot <- ggplot(CasesAvt) + 
   geom_line(aes(y=est, x=year, color=VE), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=VE), alpha = 0.1) +
-  ggtitle("Cumulative Cases Averted by Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = "Cases Averted", color = 'Scenario', fill = 'Scenario') + 
+  ggtitle("Cumulative Cases Averted per 1,000 Children\nby Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = "Cases Averted per 1,000", color = 'Scenario', fill = 'Scenario') + 
   #scale_fill_discrete(name = "Scenario") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=CasesAvt$year,breaks=CasesAvt$year) + 
-  scale_y_continuous(breaks = seq(0, 200000000, by = 20000000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 500, by = 100), labels=comma) 
 tiff("Results/Malaria_CasesAvt_byYear.tiff", units="in", width=7, height=5, res=300)
 print(CasesAvtPlot)
 dev.off()
@@ -43,11 +43,11 @@ ResCasesAvt = Malaria %>% filter(outcome %in% c('Ires_avd_'))
 ResCasesAvtPlot <- ggplot(ResCasesAvt) + 
   geom_line(aes(y=est, x=year, color=VE), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=VE), alpha = 0.1) +
-  ggtitle("Cumulative Resistant Cases Averted by Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = " Resistant Cases Averted", color = 'Scenario', fill = 'Scenario') + 
+  ggtitle("Cumulative Resistant Cases Averted per 1,000 Children\nby Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = " Resistant Cases Averted per 1,000", color = 'Scenario', fill = 'Scenario') + 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=ResCasesAvt$year,breaks=ResCasesAvt$year) + 
-  scale_y_continuous(breaks = seq(0, 2000000, by = 200000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 10, by = 1), labels=comma) 
 tiff("Results/Malaria_ResCasesAvt_byYear.tiff", units="in", width=7, height=5, res=300)
 print(ResCasesAvtPlot)
 dev.off()
@@ -57,11 +57,11 @@ DeathsAvt = Malaria %>% filter(outcome %in% c('D_avd_'))
 DeathsAvtPlot <- ggplot(DeathsAvt) + 
   geom_line(aes(y=est, x=year, color=VE), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=VE), alpha = 0.1) +
-  ggtitle("Cumulative Deaths Averted by Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = "Deaths Averted", color = 'Scenario', fill = 'Scenario') + 
+  ggtitle("Cumulative Deaths Averted per 1,000 Children\nby Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = "Deaths Averted per 1,000", color = 'Scenario', fill = 'Scenario') + 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=DeathsAvt$year,breaks=DeathsAvt$year) + 
-  scale_y_continuous(breaks = seq(0, 500000, by = 50000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 2, by = .25), labels=comma) 
 tiff("Results/Malaria_DeathsAvt_byYear.tiff", units="in", width=7, height=5, res=300)
 print(DeathsAvtPlot)
 dev.off()
@@ -71,12 +71,12 @@ CasesAll = Malaria %>% filter(outcome %in% c('I_'))
 CasesAllPlot <- ggplot(CasesAll) + 
   geom_line(aes(y=est, x=year, color=VE), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=VE), alpha = 0.1) +
-  ggtitle("Cumulative Cases by Baseline and Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = "Cases", color = 'Scenario', fill = 'Scenario') + 
+  ggtitle("Cumulative Cases per 1,000 Children by Baseline and Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = "Cases per 1,000", color = 'Scenario', fill = 'Scenario') + 
   #scale_fill_discrete(name = "Scenario") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=CasesAll$year,breaks=CasesAll$year) + 
-  scale_y_continuous(breaks = seq(0, 1000000000, by = 50000000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 2000, by = 200), labels=comma) 
 tiff("Results/Malaria_CasesAll_byYear.tiff", units="in", width=7, height=5, res=300)
 print(CasesAllPlot)
 dev.off()
@@ -86,11 +86,11 @@ ResCasesAll = Malaria %>% filter(outcome %in% c('Ires_'))
 ResCasesAllPlot <- ggplot(ResCasesAll) + 
   geom_line(aes(y=est, x=year, color=VE), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=VE), alpha = 0.1) +
-  ggtitle("Cumulative Resistant Cases by Baseline and Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = " Resistant Cases", color = 'Scenario', fill = 'Scenario') + 
+  ggtitle("Cumulative Resistant Cases per 1,000 Children by Baseline and Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = " Resistant Cases per 1,000", color = 'Scenario', fill = 'Scenario') + 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=ResCasesAll$year,breaks=ResCasesAll$year) + 
-  scale_y_continuous(breaks = seq(0, 10000000, by = 500000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 20, by = 2), labels=comma) 
 tiff("Results/Malaria_ResCasesAll_byYear.tiff", units="in", width=7, height=5, res=300)
 print(ResCasesAllPlot)
 dev.off()
@@ -100,27 +100,27 @@ DeathsAll = Malaria %>% filter(outcome %in% c('D_'))
 DeathsAllPlot <- ggplot(DeathsAll) + 
   geom_line(aes(y=est, x=year, color=VE), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=VE), alpha = 0.1) +
-  ggtitle("Cumulative Deaths by Baseline and Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = "Deaths Averted", color = 'Scenario', fill = 'Scenario') + 
+  ggtitle("Cumulative Deaths per 1,000 Children by Baseline and Vaccine Efficacy (VE) Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = "Deaths Averted per 1,000", color = 'Scenario', fill = 'Scenario') + 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=DeathsAll$year,breaks=DeathsAll$year) + 
-  scale_y_continuous(breaks = seq(0, 10000000, by = 200000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 5, by = 1), labels=comma) 
 tiff("Results/Malaria_DeathsAll_byYear.tiff", units="in", width=7, height=5, res=300)
 print(DeathsAllPlot)
 dev.off()
 
-# Cumulative resistant cases by treatment failure rate (TFR) scenario
+# Cumulative resistant cases averted by treatment failure rate (TFR) scenario
 CasesAvt2 = Malaria %>% filter(outcome %in% c('Ires_avd_','Ires2_avd_'), VE %in% c('VE1'))
 CasesAvt2$outcome[CasesAvt2$outcome %in% "Ires_avd_"] <- "Constant TFR"
 CasesAvt2$outcome[CasesAvt2$outcome %in% "Ires2_avd_"] <- "TFR increases to 80%\nby 2030"
 CasesAvt2Plot <- ggplot(CasesAvt2) + 
   geom_line(aes(y=est, x=year, color=outcome), size=.75) +
   geom_ribbon(aes(ymin=min, ymax=max, x=year, fill=outcome), alpha = 0.1) +
-  ggtitle("Cumulative Resistant Cases Averted Under Two Resistance Scenarios\nWHO Africa Region 2021-2030") + 
-  labs(x = "Year", y = "Resistant Cases Averted", color = 'Resistance Scenario', fill = 'Resistance Scenario') + 
+  ggtitle("Cumulative Resistant Cases Averted per 1,000 Children\nby Drug Resistance Scenario\nWHO Africa Region 2021-2030") + 
+  labs(x = "Year", y = "Resistant Cases Averted per 1,000", color = 'Resistance Scenario', fill = 'Resistance Scenario') + 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
   scale_x_continuous(labels=CasesAvt2$year,breaks=CasesAvt2$year) + 
-  scale_y_continuous(breaks = seq(0, 10000000, by = 1000000), labels=comma) 
+  scale_y_continuous(breaks = seq(0, 20, by = 2), labels=comma) 
 tiff("Results/Malaria_ResCases_byTFR.tiff", units="in", width=7, height=5, res=300)
 print(CasesAvt2Plot)
 dev.off()
--- Code/Malaria_Map_ResCases.R
+++ Code/Malaria_Map_ResCases.R
@@ -1,6 +1,3 @@
-# Map of resistant cases averted by country for Malaria Vaccine-Averted Burden
-# Created by Gary Lin and Alisa Hamilton
-
 library(rgdal)
 library(ggplot2)
 library(broom)
@@ -8,26 +5,26 @@ library(dplyr)
 library(data.table)
 library(viridis)
 
-setwd("[Main file path]/Code/")
+setwd("/Users/alisahamilton/Library/CloudStorage/OneDrive-CenterforDiseaseDynamics,Economics&Policy/HIV Malaria Vaccine/2. Code/")
 
 AvertData <- fread("Results/Malaria_ByCountry.csv") # Load Simulation Data
-AfrPop <- fread("Data/CreateChoroplethPlotFiles/pop.csv",header = T) # Load Population Data
+AfrPop <- fread("Data/CreateChoroplethPlotFiles/Pop1_byCountry.csv",header = T) # Load Population Data
 AfrShape <- readOGR("Data/CreateChoroplethPlotFiles/afr_g2014_2013_0/afr_g2014_2013_0.shp")  # Load Shape File
 
 AfrShape@data$id <- rownames(AfrShape@data) # Assign unique ID to merge
 AfrShape@data <- AfrShape@data %>%                          
   left_join(AvertData) %>% 
   left_join(AfrPop, by = c("ISO3" = "Country Code")) # Combine Sim and Pop Data with Shapefile Data
-AfrShape@data$ResCases_averted_pt <- AfrShape@data$Ires_VE1_avd / AfrShape@data[,"2011"] * 1000 # Calculate density (per thousand persons)
-AfrShape@data$ResCases_averted_pt_min <- AfrShape@data$Ires_VE1_avd_min / AfrShape@data[,"2011"] * 1000
-AfrShape@data$ResCases_averted_pt_max <- AfrShape@data$Ires_VE1_avd_max / AfrShape@data[,"2011"] * 1000
+AfrShape@data$ResCases_averted_pt <- AfrShape@data$Ires_avd_VE1 / AfrShape@data[,"Pop1"] * 1000 # Calculate density (per thousand persons)
+AfrShape@data$ResCases_averted_pt_min <- AfrShape@data$Ires_avd_VE1_min / AfrShape@data[,"Pop1"] * 1000
+AfrShape@data$ResCases_averted_pt_max <- AfrShape@data$Ires_avd_VE1_max / AfrShape@data[,"Pop1"] * 1000
 AfrData <- fortify(AfrShape) %>% left_join(AfrShape@data) # Convert shape file object to data.frame for ggplot
 
 ResCases_avertedplot <- ggplot() + 
   geom_polygon(data = AfrData, aes(long, y=lat, group=group, fill=ResCases_averted_pt), color="white") +
   coord_fixed() +
   # geom_text(data=AfrData, aes(x=long, y=lat, label=ISO3), color="white", size=3, alpha=0.6) +
-  scale_fill_viridis(option="plasma", name = "Resistant Cases Averted\nPer 1,000 Population") +
+  scale_fill_viridis(option="plasma", name = "Resistant Cases Averted\nPer 1,000 Children") +
   ggtitle( "Resistant Cases Averted per 1,000 Population by Country, WHO Africa Region (2021-2030)") +
   theme_void() +
   theme(plot.title = element_text(hjust = 0.5))
