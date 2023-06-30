ca8f64b27eb0a76ca82dc2fba39329f27bf89618
Alisa Hamilton <67701544+hamilton89@users.noreply.github.com>
2023-06-30 11:23:31 -0400

    Update Malaria_PE.py

--- Code/Malaria_PE.py
+++ Code/Malaria_PE.py
@@ -35,10 +35,10 @@ data1.loc[data1['year'] <= 2024, 'inc_byage_min'] = data1['malaria_prev_min'] * 
 data1.loc[data1['year'] <= 2030, 'inc_byage_max'] = data1['malaria_prev_max'] * data1['5_9prop_est'] / data1['Pop5_9']
 data1.loc[data1['year'] <= 2024, 'inc_byage_max'] = data1['malaria_prev_max'] * data1['1_4prop_est'] / data1['Pop1_4']
 
-#country parameters - Supplementary Tables 4 and 5
+#country parameters - Supplementary Table 4
 country_rates = data1.loc[(data1['year'] == 2021)]
 country_rates = country_rates[['country', 'at_risk', 'coverage','malaria_prev','malaria_prev_min','malaria_prev_max',
-                               '1_4est','5_9est','Allages_est','Pop1_4','Pop5_9','inc_byage','inc_byage_min','inc_byage_max', 'trr','tfr','CFR_malaria']].set_index('country')
+                               '1_4est','5_9est','Allages_est','Pop1_4','Pop5_9','inc_byage','inc_byage_min','inc_byage_max', 'trr','pcr','tfr','CFR_malaria']].set_index('country')
 country_rates = country_rates.rename(columns={'malaria_prev':'WHOcases','malaria_prev_min':'WHOcases_min','malaria_prev_max':'WHOcases_max',
                                               '1_4est':'IHME1_4cases','5_9est':'IHME5_9cases','Allages_est':'IHMEallcases',
                                               'inc_byage':'inc1_4','inc_byage_min':'inc1_4min','inc_byage_max':'inc1_4max' })
@@ -48,13 +48,13 @@ inc5_9 = inc5_9.rename(columns={'inc_byage':'inc5_10','inc_byage_min':'inc5_10mi
 country_rates = country_rates.merge(inc5_9,how='left',on='country')
 country_rates = country_rates[['country', 'at_risk', 'coverage','WHOcases','WHOcases_min','WHOcases_max',
                                'IHME1_4cases','IHME5_9cases','IHMEallcases','Pop1_4','Pop5_9','inc1_4','inc1_4min','inc1_4max','inc5_10','inc5_10min','inc5_10max', 
-                               'trr','tfr','CFR_malaria']].sort_values(by='country')
+                               'trr','pcr','tfr','CFR_malaria']].sort_values(by='country')
 country_rates.to_csv(OneDrive + 'Results/Malaria_Country_Parameters.csv')
 
 #preprocessing
 data1 = data1.loc[(data1['year'] != 2020)]
 data1['VE0'] = 0 # Baseline - no vaccine 
-data1 = data1[['country','year','Pop1', 'coverage', 'inc_byage','trr','tfr','tfr_increasing','CFR_malaria','VE0', 'VE1', 'VE2', 'VE3']]
+data1 = data1[['country','year','Pop1', 'coverage', 'inc_byage','trr','pcr','pcr_increasing','CFR_malaria','VE0', 'VE1', 'VE2', 'VE3']]
 countries = list(data1['country'].unique())
 
 ve_lst = []
@@ -62,14 +62,9 @@ for ve in scenarios:
     country_lst = []
     for country in countries:
         data = data1.loc[data1['country'] == country]
-        #data = data1.loc[data1['country'] == "Angola"]
-        #ve = 'VE0'
         data['scenario'] = ve
-        data.loc[data['scenario'] == 'VE0', 'coverage'] = 0
-        #data['CFR_malaria'] = .0029  
-        #cfr = data['CFR_malaria']               
+        data.loc[data['scenario'] == 'VE0', 'coverage'] = 0             
         data['VE'] = data[ve]
-        #data.loc[data['VE'] == 0, 'coverage'] = 0
         data['cohort'] = np.r_[:len(data)] % 10 + 1
         data['cohort'] = data['cohort']
         data_temp = data[['year', 'Pop1', 'cohort']]
@@ -86,7 +81,6 @@ for ve in scenarios:
         cohorts = [1,2,3,4,5,6,7,8,9,10]
         cohort_lst = []
         for cohort in cohorts:
-            #cohort = 1
             data2 = data.loc[data['cohort'] == cohort]       
             data2['P_vax'] = data2['Pop1'] * data2['coverage'] * data2['VE']                                    
             data2.loc[data2['year'] == 2021, 'uP_vax'] = data2['Pop1'] * data2['coverage'] - data2['P_vax']
@@ -113,8 +107,8 @@ for ve in scenarios:
     malaria = pd.concat(country_lst).sort_values(by=['country', 'cohort', 'year'])
     malaria['I'] = malaria['I_vax'] + malaria['I_novax']
     malaria['D'] = malaria['D_vax'] + malaria['D_novax']
-    malaria['Ires'] = malaria['I'] * malaria['trr'] * malaria['tfr']
-    malaria['Ires2'] = malaria['I'] * malaria['trr'] * malaria['tfr_increasing']
+    malaria['Ires'] = malaria['I'] * malaria['trr'] * malaria['pcr']
+    malaria['Ires2'] = malaria['I'] * malaria['trr'] * malaria['pcr_increasing']
     malaria = malaria.loc[malaria['year'] <=2030]
     malaria = malaria.groupby(['country', 'year'])[['I', 'D', 'Ires', 'Ires2']].sum().reset_index()
     malaria['scenario'] = ve
@@ -127,9 +121,10 @@ final = final[['country', 'year', 'I_VE0', 'D_VE0', 'Ires_VE0', 'Ires2_VE0',
                                   'I_VE1', 'D_VE1', 'Ires_VE1', 'Ires2_VE1',
                                   'I_VE2', 'D_VE2', 'Ires_VE2', 'Ires2_VE2',
                                   'I_VE3', 'D_VE3', 'Ires_VE3', 'Ires2_VE3']]
-#final.to_csv(OneDrive + 'Results/Malaria_PE.csv')
+final.to_csv(OneDrive + 'Results/Malaria_PE.csv')
    
 print("--- %s seconds ---" % (time.time() - start_time)) 
+   
    
         
         
